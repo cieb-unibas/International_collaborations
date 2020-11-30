662b30ae07a0a9294e78fad70e18a61e75a15b90
Dragan Filimonovic <fildra00@service06.cluster.bc2.ch>
2020-11-30 15:40:36 +0100

    Creating data for the analysis on international collaborations

---
+++ data_prep.R
@@ -0,0 +1,70 @@
+print("International collaborations and quality of patents")
+# Last modified 30.11.2020 / DF
+
+require(data.table)
+require(plyr)
+library(dplyr)
+require(ggplot2)
+library(countrycode)
+library(viridis)
+library(reshape2)
+library(stringr)
+library("rio")
+library(fastDummies)
+library(splitstackshape)
+rm(list = ls())
+
+################################################################################
+# Creating data from multiple datasets # 
+################################################################################
+
+# Preparing inventor data
+################################################################################
+  inventors_data <- readRDS("/scicore/home/weder/GROUP/Innovation/01_patent_data/created data/inv_reg_CHcommute_adj.rds")
+  inventors_data2 <-subset(inventors_data, select = c("p_key", "patent_id", "regio_inv", "ctry_inv", "ipc_main", "tech_field"))
+
+# Making var containing all inventor countries by patents
+  inventors_data3 <- inventors_data2 %>%
+                      group_by(p_key, patent_id) %>%
+                      mutate(ctry_inventor = paste(ctry_inv, collapse = "_")) 
+
+# Keeping only first observation
+  inventors_data_final <- inventors_data3 %>% 
+                        group_by(p_key, patent_id) %>% 
+                        slice(1) %>%
+                        ungroup()
+
+# Preparing legal owner data
+################################################################################
+  owner_data <- readRDS("/scicore/home/weder/GROUP/Innovation/01_patent_data/created data/firm_reg.rds")
+  owner_data2 <-subset(owner_data, select = c("p_key","pub_nbr", "country"))
+  colnames(owner_data2) <- c("p_key", "patent_id", "ctry_owner")
+
+# Making var containing all legal owner countries by patents
+  owner_data3 <- owner_data2 %>%
+                 group_by(p_key, patent_id) %>%
+                 mutate(ctry_leg_owner = paste(ctry_owner, collapse = "_")) 
+
+# Keeping only first observation
+  owner_data_final <- owner_data3 %>% 
+                      group_by(p_key, patent_id) %>% 
+                      slice(1) %>%
+                      ungroup()
+
+# Preparing citation data
+################################################################################
+  forcit_data <- readRDS("/scicore/home/weder/GROUP/Innovation/01_patent_data/created data/pat_dat.rds")
+  forcit_data2 <-subset(forcit_data, select = c("p_key","pub_nbr_oecd", "world_class_90", "world_class_99"))
+  colnames(forcit_data2) <- c("p_key", "patent_id", "world_class_90", "world_class_99")
+  
+# Merging all three datasets together
+################################################################################
+  
+  inv_owner_dat <- merge(inventors_data_final, owner_data_final)  
+  int_collab_dat <- merge(inv_owner_dat, forcit_data2)  
+  
+  int_collab_dat_final <-subset(int_collab_dat, select = c("p_key","patent_id", "ipc_main", "tech_field", "ctry_inventor", "ctry_leg_owner", "world_class_90", "world_class_99"))
+  
+  #SAVING THE FINAL DATASET
+  saveRDS(object=int_collab_dat_final, file = "/scicore/home/weder/GROUP/Innovation/01_patent_data/created data/int_collab_dat_final.rds")
+  
