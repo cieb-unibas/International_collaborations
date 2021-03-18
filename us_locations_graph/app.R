2da06e03178925e34c4900ba7464a24e7366506f
Dragan Filimonovic <fildra00@service06.cluster.bc2.ch>
2021-03-18 11:50:57 +0100

    Finalization of the US location trends graph

--- us_locations_graph/app.R
+++ us_locations_graph/app.R
@@ -21,8 +21,8 @@ rm(list = ls())
 
 us_locations <- readRDS("/scicore/home/weder/GROUP/Innovation/01_patent_data/created data/inv_coll_us_networkdata.rds")
 
-us_locations<-us_locations %>%
-  filter(location !="US")
+#us_locations<-us_locations %>%
+# filter(location !="US")
 
 
 ui <- fluidPage(
@@ -47,45 +47,45 @@ server <- function(input, output,session) {
     }) 
     
     output$coolplot <- renderPlotly({
-
-fig <- plot_ly(
-  dr(), x = ~location, y = ~share, frame=~interval,
-  color=~techbroad, type = "bar",
-  mode="markers", 
-  text = ~paste(sep='','<br>Share of inventors:', round(`share`,1),'%',
-                '<br>Location:', `location_name`)) %>%
-  layout(
-    title="Foreign collaboration and citation",
-    
-    yaxis = list(title = '% of inventors',
-                 gridcolor = 'white',
-                 
-                 range=c(0,20),
-                 zerolinewidth = F,
-                 ticklen = 5,
-                 gridwidth = 2),
-    xaxis = list(title = 'Locations',
-                 gridcolor = 'white',
-                 zerolinewidth = F,
-                 type = 'location',
-                 range = c('location'),
-                 ticklen = 5,
-                 tickvals = dr()$location,
-                 gridwith = 1),
-    paper_bgcolor = 'white',
-    plot_bgcolor = 'white') %>%
-  animation_opts(
-    2500, redraw = FALSE
-  ) %>%
-  
-  animation_slider(
-    currentvalue = list(prefix = "", font = list(color="black"))
-  )
-
-fig <- fig %>% layout(title = 'Locations of US patent inventors',
-                      xaxis = list(showgrid = TRUE),
-                      legend.position = "none",
-                      yaxis = list(showgrid = TRUE))
+      
+      fig <- plot_ly(
+        dr(), x = ~location, y = ~share, frame=~interval,
+        color=~techbroad, type = "bar",
+        mode="markers", 
+        text = ~paste(sep='','<br>Share of inventors:', round(`share`,1),'%',
+                      '<br>Location:', `location_name`)) %>%
+        layout(
+          title="Foreign collaboration and citation",
+          
+          yaxis = list(title = '% of inventors',
+                       gridcolor = 'white',
+                       
+                       range=c(0,20),
+                       zerolinewidth = F,
+                       ticklen = 5,
+                       gridwidth = 2),
+          xaxis = list(title = 'Locations',
+                       gridcolor = 'white',
+                       zerolinewidth = F,
+                       type = 'location',
+                       range = c('location'),
+                       ticklen = 5,
+                       tickvals = dr()$location,
+                       gridwith = 1),
+          paper_bgcolor = 'white',
+          plot_bgcolor = 'white') %>%
+        animation_opts(
+          2500, redraw = FALSE
+        ) %>%
+        
+        animation_slider(
+          currentvalue = list(prefix = "", font = list(color="black"))
+        )
+      
+      fig <- fig %>% layout(title = 'Locations of US patent inventors',
+                            xaxis = list(showgrid = TRUE),
+                            legend.position = "none",
+                            yaxis = list(showgrid = TRUE))
     })
   })  
 }
