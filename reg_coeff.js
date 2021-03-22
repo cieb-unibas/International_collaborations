1d804cb0901f0ab9c5dcdb7d48b0bc6425556ae4
ch54321 <christian.rutzer@unibas.ch>
2021-03-22 10:43:47 +0100

    works with two variables

--- reg_coeff.js
+++ reg_coeff.js
@@ -18,7 +18,14 @@ var     var1 = unpack(rows, 'term'),
         currentctry = [],
         currentmodel = [],
         currentconf_l = [],
-        currentconf_h = [];
+        currentconf_h = [],
+        current_ctry_model = [],
+        current_model_1 = [],
+        current_model_2 = [],
+        current_model_3 = [],
+        current_model_4 = [],
+        current_model_5 = [],
+        current_model_6 = [];
 
     for (var i = 0; i < var2.length; i++ ){
         if (listVar2.indexOf(var2[i]) === -1 ){
@@ -40,28 +47,38 @@ var     var1 = unpack(rows, 'term'),
         currentconf_h = [];
         currentctry = [];
         currentmodel = [];
+        current_ctry_model = [];
+        current_model_1 = [];
+        current_model_2 = [];
+        current_model_3 = [];
+        current_model_4 = [];
+        current_model_5 = [];
+        current_model_6 = [];
+        
         for (var i = 0 ; i < var1.length ; i++){
           for(var j = 0; j < var2.length; j ++){
-            if (var1[i] === chosenCountry[j] && var2[i] === chosenCountry2) {
+          for(var k = 0; k < var1.length; k++){
+            if (var1[i] === chosenCountry[j] && var2[i] === chosenCountry2[k]) {
                 currentest.push(est[i]);
                 currentctry.push(var1[i]);
                 currentmodel.push(var2[i]);
                 currentconf_l.push(conf_l[i]);
                 currentconf_h.push(conf_h[i]);
+                current_ctry_model.push(var1[i] + " " + var2[i]);
                 }
     }
     }
 }
-
+}
 // Default Country Data
-setBubblePlot("CN", "Overall");
+setBubblePlot(["CN", "DE"], "Overall");
 
 function setBubblePlot(chosenCountry, chosenCountry2) {
         getCountryData(chosenCountry, chosenCountry2);
 
         var trace1 = {
             x: currentest,
-            y: currentctry,
+            y: current_ctry_model,
             error_x: {
             type: 'data',
             array: currentconf_h,
@@ -84,9 +101,32 @@ function setBubblePlot(chosenCountry, chosenCountry2) {
             hoverinfo: 'none'
         };
         
-      
+            var trace2 = {
+            x: currentest,
+            y: current_ctry_model,
+            error_x: {
+            type: 'data',
+            array: currentconf_h,
+            visible: true},
+         //    customdata: currentmedian,
+         //    text: currentVarName,
+            type: 'scatter',
+            mode: "markers",
+            textposition: "auto",
+            orientation: 'h',
+         //    transforms: [{
+         //    type: 'sort',
+         //    target: 'y',
+         //    order: 'descending',
+         //    }], 
+       //      hovertemplate:  '<b>%{text}</b>' + 
+      //                        '<br><b>Arithmetisches Mittel: %{x}</b>' + '<br><b>Median: %{customdata}</b>' + '<extra></extra>',
+            marker: { color:  'rgba(53,91,118,0.8)', size: 20},
+            textposition: 'center',
+            hoverinfo: 'none'
+        };
 
-var data = [trace1];
+var data = [trace1, trace2];
  
         var layout = {
           hovermode: "closest",
@@ -105,15 +145,17 @@ var data = [trace1];
     yaxis: {fixedrange: true,
             zeroline: false,
             tickfont: {size: 18, width: 2},
-            title: {text: '<b></b>'}},
-    shapes: [{
-      type: 'line',
-      x0: 0, 
-      y0: currentctry.length,
-      x1: 0,
-      y1: currentctry.length,
-      line: {dash: 'dot'}
-    }]};
+            title: {text: '<b></b>'}}
+ //   shapes: [{
+//      type: 'line',
+//      x0: 0, 
+//      y0: currentctry.length,
+//      x1: 0,
+//      y1: currentctry.length,
+//      line: {dash: 'dot'}
+//    }]
+  
+};
   
 
 Plotly.newPlot('myDiv', data, layout, {displayModeBar: false});
@@ -141,11 +183,17 @@ var countrySelector2 = document.getElementById("select_2");
 
 function updateCountry(){
   var var_2l = [];
+  var var_1l = [];
   $.each($("#select_1 option:selected" ), function() {
   var_2l.push($(this).val());  
   })
-  document.getElementById("test1").innerHTML = var_2l; 
-        setBubblePlot(var_2l, countrySelector2.value);
+  
+    $.each($("#select_2 option:selected" ), function() {
+  var_1l.push($(this).val());  
+  })
+  
+  document.getElementById("test1").innerHTML = var_1l; 
+        setBubblePlot(var_2l, var_1l);
     }
 
 
